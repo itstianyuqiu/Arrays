ce6b85589791cf82c7897410fa8d5248148d3eba
Yu-Cheng Tu <yu-cheng.tu@auckland.ac.nz>
2017-11-09 09:27:16 +1300

    Materials for lab 04

---
+++ src/ictgradschool/industry/lab04/ex03/GuessingGame.java
@@ -0,0 +1,22 @@
+package ictgradschool.industry.lab04.ex03;
+
+/**
+ * A guessing game!
+ */
+public class GuessingGame {
+
+    public void start() {
+
+        // TODO Write your code here.
+    }
+
+    /**
+     * Program entry point. Do not edit.
+     */
+    public static void main(String[] args) {
+
+        GuessingGame ex = new GuessingGame();
+        ex.start();
+
+    }
+}
